0f8ffc5782b7b95681710caf0eb5dddc2455b7cd
jorge calderon gonzalez <jcalderoxp@alumnos.unex.es>
2023-11-07 17:05:55 +0100

    rooms limpio

--- rooms/cmake-build-debug/src/CMakeFiles/ICES__home_robocomp_robocomp_components_grupo5-robotica2023_rooms_src.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICES__home_robocomp_robocomp_components_grupo5-robotica2023_rooms_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
